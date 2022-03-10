4ccc633794c8d953d40c5850443f7d85e095b073
Giovanny Aguilar <haguilerts54@gmail.com>
2022-03-10 02:23:05 -0300

    añadiendo una BBDD oneline db4free.net

--- src/keys.ts
+++ src/keys.ts
@@ -1,11 +1,18 @@
 export default {
     Database:{
+        host: 'db4free.net',
+        user: 'haguilerts',
+        password: '121qseawd1',
+        port: 3306,
+        database:'bdmysql22'
+    }
+   /*  Database:{
         host: 'localhost',
         user: 'root',
         password: '',
         port: 8081,
         database:'ng_games_db'
-    }
+    } */
   /*   Database:{
         host: 'remotemysql.com',
         user: 'PPybBC1CL5',
